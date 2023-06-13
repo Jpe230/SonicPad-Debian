2efebc20199d284951f591b4c280214d4b88a9ad
jpe230 <pablin.123.ra@gmail.com>
2023-06-12 22:47:23 -0600

    Update script for Pi build host

--- src/create_rootfs.sh
+++ src/create_rootfs.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
 
-install_utils()
-{
-    apt update && 
-    apt install git net-tools build-essential locales openssh-server wget libssl-dev sudo network-manager systemd-timesyncd -y
-}
-export -f install_utils
-
 if [ "$EUID" -ne 0 ]
   then echo "Please run as root"
   exit
 fi
 
+source spinner.sh
 source .config
 
 # Validate directories
@@ -50,38 +44,58 @@ then
 	fi
 fi
 
-echo "Removin old rootfs..."
+start_spinner "Removing old rootfs"
 rm -rf $ROOTFS_DIR
+stop_spinner
 
 # 1) Create a basic rootfs
-echo "Creating a basic rootfs..."
+start_spinner "Creating a basic rootfs"
 {
     apt-get update
-    apt-get install qemu-user-static debootstrap -y
+    #apt-get install qemu-user-static -y
+    apt-get install debootstrap -y # Install only debootstrap, pi doesnt need it
     debootstrap --no-check-gpg --foreign --verbose --arch=armhf $DEB_DISTRO $ROOTFS_DIR $DEB_URL
-    cp /usr/bin/qemu-arm-static $ROOTFS_DIR/usr/bin/
-    chmod +x $ROOTFS_DIR/usr/bin/qemu-arm-static
+    #cp /usr/bin/qemu-arm-static $ROOTFS_DIR/usr/bin/
+    #chmod +x $ROOTFS_DIR/usr/bin/qemu-arm-static
 } &> /dev/null
+stop_spinner
 
 
 # 2) Run second stage bootstrao on rootfs
-echo "Running second stage..."
+start_spinner "Running second stage"
 {
     LC_ALL=C LANGUAGE=C LANG=C chroot $ROOTFS_DIR /debootstrap/debootstrap --second-stage --verbose
 } &> /dev/null
+stop_spinner
+
+# # 3) Installing packages
+start_spinner "Installing packages"
+{
+    LC_ALL=C LANGUAGE=C LANG=C chroot $ROOTFS_DIR apt update
+    LC_ALL=C LANGUAGE=C LANG=C chroot $ROOTFS_DIR apt install git net-tools build-essential locales openssh-server wget libssl-dev sudo network-manager systemd-timesyncd -y
+} &> /dev/null
+stop_spinner
 
 
-# 3) Copy our base filesystem
-echo "Copying base rootfs..."
-cp -r $BASEFS_DIR/etc/* $ROOTFS_DIR/etc/
-cp -r $BASEFS_DIR/usr/local/bin/* $ROOTFS_DIR/usr/local/bin/
-cp -r $BASEFS_DIR/lib/firmware/ $ROOTFS_DIR/lib/
-cp -r $BASEFS_DIR/lib/modules/ $ROOTFS_DIR/lib/
+# 4) Copy our base filesystem
+start_spinner "Copying base rootfs"
+{
+    cp -r $BASEFS_DIR/etc/* $ROOTFS_DIR/etc/
+    cp -r $BASEFS_DIR/usr/local/bin/* $ROOTFS_DIR/usr/local/bin/
+    cp -r $BASEFS_DIR/lib/firmware/ $ROOTFS_DIR/lib/
+    cp -r $BASEFS_DIR/lib/modules/ $ROOTFS_DIR/lib/
+}
+stop_spinner
 
-# 4) Create default user
-echo "Creating default user..."
+
+# 5) Create default user
+start_spinner "Creating default user"
 {
     LC_ALL=C LANGUAGE=C LANG=C chroot $ROOTFS_DIR adduser --gecos "" --disabled-password $L_USERNAME
     LC_ALL=C LANGUAGE=C LANG=C chroot $ROOTFS_DIR chpasswd <<<"$L_USERNAME:$L_PASSWORD"
+    LC_ALL=C LANGUAGE=C LANG=C chroot $ROOTFS_DIR /bin/bash -c "usermod -aG sudo $L_USERNAME"
 } &> /dev/nul
+stop_spinner
+
+echo "Done creating rootfs"
 
---
+++ src/spinner.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Taken from this repository: https://github.com/wcarhart/willcarh.art-snippets
+
+spinner_pid=
+
+function start_spinner {
+    set +m
+    echo -n "$1         "
+    { while : ; do for X in '  •     ' '   •    ' '    •   ' '     •  ' '      • ' '     •  ' '    •   ' '   •    ' '  •     ' ' •      ' ; do echo -en "\b\b\b\b\b\b\b\b$X" ; sleep 0.1 ; done ; done & } 2>/dev/null
+    spinner_pid=$!
+}
+
+function stop_spinner {
+    { kill -9 $spinner_pid && wait; } 2>/dev/null
+    set -m
+    echo -en "\033[2K\r"
+}
+
+trap stop_spinner EXIT
